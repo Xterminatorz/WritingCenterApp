e58519e7f0dadb391366d1bbea3da8d241b4dc1f
Johnny <johnny.lui@sjsu.edu>
2015-04-04 00:40:53 -0700

    FAQ page now retrieves the FAQs from the website

--- app/src/main/java/edu/sjsu/writingcenter/FAQActivity.java
+++ app/src/main/java/edu/sjsu/writingcenter/FAQActivity.java
@@ -1,17 +1,35 @@
 package edu.sjsu.writingcenter;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FAQActivity extends ActionBarActivity {
+
+    ExpandableListView expandableListView;
+    FAQExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        new FAQData().execute();
     }
 
 
@@ -37,4 +55,56 @@ public class FAQActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class FAQData extends AsyncTask<Void, Void, Void> {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<>();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(FAQActivity.this);
+            mProgressDialog.setTitle("Writing Center FAQs");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect("http://www.sjsu.edu/writingcenter/faq/index.html").get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (doc != null) {
+                Elements questions = doc.select(".content_wrapper > div:nth-child(2) > h3");
+                for (Element question : questions) {
+                    if (question.text().contains("Where is the Writing Center"))
+                        continue;
+                    List<String> text = new ArrayList<>();
+                    Element el = question.nextElementSibling();
+                    while (el != null && el.nodeName().equals("p")) {
+                        text.add(el.text());
+                        el = el.nextElementSibling();
+                        if (el != null && "ul".equals(el.nodeName()))
+                            for (Element li : el.children())
+                                text.add(li.text());
+                    }
+                    expandableListDetail.put(question.text(), text);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            expandableListView = (ExpandableListView) findViewById(R.id.FAQExpandableListView);
+            expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+            expandableListAdapter = new FAQExpandableListAdapter(FAQActivity.this, expandableListTitle, expandableListDetail);
+            expandableListView.setAdapter(expandableListAdapter);
+            mProgressDialog.dismiss();
+        }
+    }
+
 }
