c609c3162618cec239dc847a15998a333a4238b1
Alexander Wong <wong.acj@gmail.com>
2015-04-13 01:22:25 -0700

    finished implementation of location page

--- app/src/main/java/edu/sjsu/writingcenter/LocationActivity.java
+++ app/src/main/java/edu/sjsu/writingcenter/LocationActivity.java
@@ -3,11 +3,17 @@ package edu.sjsu.writingcenter;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 
-public class LocationActivity extends ActionBarActivity {
+public class LocationActivity extends ActionBarActivity implements View.OnClickListener{
     public static Typeface Font;
 
     @Override
@@ -16,8 +22,7 @@ public class LocationActivity extends ActionBarActivity {
         setContentView(R.layout.activity_location);
 
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -39,5 +44,23 @@ public class LocationActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("onclk", "Button was clicked");
+        ImageView imView1 = (ImageView) findViewById(R.id.imageView1);
+        ImageView imView2 = (ImageView) findViewById(R.id.imageView2);
+        switch(v.getId())
+        {
+            case R.id.button1:
+                imView1.setVisibility(View.VISIBLE);
+                imView2.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.button2:
+                imView1.setVisibility(View.INVISIBLE);
+                imView2.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 }
